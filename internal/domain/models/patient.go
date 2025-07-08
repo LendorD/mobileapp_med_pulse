c26cb53bdd3ea99b88c0bddf5b002a24b61192ea
maximdudko17 <maxim.dudko17@gmail.com>
2025-07-08 12:32:16 +0300

    Pull_Actual2

--- internal/domain/models/patient.go
+++ internal/domain/models/patient.go
@@ -1,7 +1,6 @@
 package models
 
 import (
-	"gorm.io/gorm"
 	"time"
 )
 
@@ -15,11 +14,4 @@ type CreatePatientRequest struct {
 	FullName  string
 	BirthDate time.Time
 	IsMale    bool
-}
-
-type ShortPatientResponse struct {
-	gorm.Model
-	FullName  string    `json:"-" example:"Смирнов Алексей Петрович" rus:"ФИО"`
-	BirthDate time.Time `json:"birth_date" example:"1980-05-15T00:00:00Z" rus:"Дата рождения"`
-	IsMale    bool      `json:"is_male" example:"true" rus:"Пол (true - мужской)"`
 }
