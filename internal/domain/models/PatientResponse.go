77a6595331c0a1095288115ef55cf51ce19930bc
LendorD <Wolf1980M@gmail.com>
2025-07-08 16:26:44 +0300

    fix all conflicts

--- internal/domain/models/PatientResponse.go
+++ internal/domain/models/PatientResponse.go
@@ -1,36 +1,32 @@
 package models
 
-import (
-	"time"
-)
+// type UpdatePatientRequest struct {
+// 	ID        uint
+// 	FullName  string
+// 	BirthDate time.Time
+// }
 
-type UpdatePatientRequest struct {
-	ID        uint
-	FullName  string
-	BirthDate time.Time
-}
+// type CreatePatientRequest struct {
+// 	FullName  string
+// 	BirthDate time.Time
+// 	IsMale    bool
+// }
 
-type CreatePatientRequest struct {
-	FullName  string
-	BirthDate time.Time
-	IsMale    bool
-}
+// type ShortPatientResponse struct {
+// 	ID        uint      `json:"id"`
+// 	FullName  string    `json:"full_name"`
+// 	BirthDate time.Time `json:"birth_date"`
+// 	IsMale    bool      `json:"is_male"`
+// }
 
-type ShortPatientResponse struct {
-	ID        uint      `json:"id"`
-	FullName  string    `json:"full_name"`
-	BirthDate time.Time `json:"birth_date"`
-	IsMale    bool      `json:"is_male"`
-}
-
-type FullPatientResponse struct {
-	ID         uint      `json:"id"`
-	FirstName  string    `json:"first_name"`
-	Surname    string    `json:"surname"`
-	MiddleName string    `json:"middle_name"`
-	FullName   string    `json:"full_name"`
-	BirthDate  time.Time `json:"birth_date"`
-	IsMale     bool      `json:"is_male"` // true - мужской, false - женский
-	SNILS      string    `json:"snils"`   // СНИЛС
-	OMS        string    `json:"oms"`     // Полис ОМС
-}
+// type FullPatientResponse struct {
+// 	ID         uint      `json:"id"`
+// 	FirstName  string    `json:"first_name"`
+// 	Surname    string    `json:"surname"`
+// 	MiddleName string    `json:"middle_name"`
+// 	FullName   string    `json:"full_name"`
+// 	BirthDate  time.Time `json:"birth_date"`
+// 	IsMale     bool      `json:"is_male"` // true - мужской, false - женский
+// 	SNILS      string    `json:"snils"`   // СНИЛС
+// 	OMS        string    `json:"oms"`     // Полис ОМС
+// }
