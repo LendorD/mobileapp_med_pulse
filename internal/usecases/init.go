dba970fca23cc3e1547afbce37fa1bebdfe65d15
ALYoNka <khrllw@gmail.com>
2025-07-08 14:35:55 +0300

    patient handlers

--- internal/usecases/init.go
+++ internal/usecases/init.go
@@ -2,34 +2,21 @@ package usecases
 
 import (
 	"github.com/AlexanderMorozov1919/mobileapp/internal/config"
-	"github.com/AlexanderMorozov1919/mobileapp/internal/domain/entities"
 	_ "github.com/AlexanderMorozov1919/mobileapp/internal/domain/entities"
 	"github.com/AlexanderMorozov1919/mobileapp/internal/interfaces"
-	"github.com/AlexanderMorozov1919/mobileapp/pkg/errors"
 	_ "github.com/AlexanderMorozov1919/mobileapp/pkg/errors"
 )
 
 type UseCases struct {
-}
-
-func (u UseCases) AddAllergyToPatient(patientID, allergyID uint, description string) (entities.PatientsAllergy, *errors.AppError) {
-	//TODO implement me
-	panic("implement me")
-}
-
-func (u UseCases) GetAllergyByPatientID(patientID uint) ([]entities.Allergy, *errors.AppError) {
-	//TODO implement me
-	panic("implement me")
-}
-
-func (u UseCases) RemoveAllergyFromPatient(patientID, allergyID uint) *errors.AppError {
-	//TODO implement me
-	panic("implement me")
-}
-
-func (u UseCases) UpdateAllergyDescription(patientID, allergyID uint, description string) (entities.PatientsAllergy, *errors.AppError) {
-	//TODO implement me
-	panic("implement me")
+	interfaces.AllergyUsecase
+	interfaces.ContactInfoUsecase
+	interfaces.DoctorUsecase
+	interfaces.EmergencyReceptionUsecase
+	interfaces.EmergencyReceptionMedServicesUsecase
+	interfaces.MedServiceUsecase
+	interfaces.PatientUsecase
+	interfaces.PersonalInfoUsecase
+	interfaces.ReceptionUsecase
 }
 
 func NewUsecases(r interfaces.Repository, s interfaces.Service, conf *config.Config) interfaces.Usecases {
