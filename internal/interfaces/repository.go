fc335f428893212fe15879d2d03d97bd55bb789c
maximdudko17 <maxim.dudko17@gmail.com>
2025-07-18 12:11:33 +0300

    SMPS_FIX_ANR_ROUTES

--- internal/interfaces/repository.go
+++ internal/interfaces/repository.go
@@ -81,7 +81,7 @@ type ReceptionSmpRepository interface {
 	UpdateReceptionSmp(id uint, updateMap map[string]interface{}) (uint, error)
 	DeleteReceptionSmp(id uint) error
 
-	GetReceptionWithMedServicesByID(id uint) (entities.ReceptionSMP, error)
+	GetReceptionWithMedServicesByID(id uint, emergencyCallID uint) (entities.ReceptionSMP, error)
 	GetReceptionSmpByID(id uint) (entities.ReceptionSMP, error)
 	GetReceptionSmpByDoctorID(doctorID uint) ([]entities.ReceptionSMP, error)
 	GetReceptionSmpByPatientID(patientID uint) ([]entities.ReceptionSMP, error)
