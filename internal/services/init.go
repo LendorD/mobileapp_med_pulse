50b817159676c92a3e728248442137f196c3eac5
LendorD <dlucenko@kvant-is.ru>
2025-10-26 20:16:27 +0300

    work

--- internal/services/init.go
+++ internal/services/init.go
@@ -1,18 +1,28 @@
 package services
 
 import (
+	"log"
+
+	"github.com/AlexanderMorozov1919/mobileapp/internal/config"
 	"github.com/AlexanderMorozov1919/mobileapp/internal/interfaces"
 )
 
 type Service struct {
 	interfaces.ParamsParserService
 	interfaces.FilterBuilderService
+	interfaces.ImageService
 }
 
-func NewService() interfaces.Service {
+func NewService(cfg *config.Config) interfaces.Service {
 	parser := NewParamsParser()
+	imageSvc, err := NewImageService(cfg.MinIO)
+	if err != nil {
+		log.Fatalf("Failed to initialize ImageService: %v", err)
+	}
+
 	return Service{
-		parser,
-		NewFilterBuilder(parser),
+		ParamsParserService:  parser,
+		FilterBuilderService: NewFilterBuilder(parser),
+		ImageService:         imageSvc,
 	}
 }
